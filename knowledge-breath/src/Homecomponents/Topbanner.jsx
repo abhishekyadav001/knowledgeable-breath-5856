1136ba8cd3e5f87b3e3143370ebbce018a24e266
abhishekyadav <yadavabhishek2723@gmail.com>
2022-09-29 11:58:09 +0530

    Day-3 start works

--- knowledge-breath/src/Homecomponents/Topbanner.jsx
+++ knowledge-breath/src/Homecomponents/Topbanner.jsx
@@ -2,10 +2,12 @@ import { Carousel } from 'react-responsive-carousel';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Stack } from '@chakra-ui/react';
 
 
 export default function Topbanner(){
     return (
+        <Stack position="relative">
         <Carousel width={"100%"} showThumbs={false} autoPlay={true}showIndicators={false} showStatus={false} infiniteLoop={true} interval={1300} >
         <div  >
             <img src="https://cdn.shopify.com/s/files/1/0052/7551/6995/files/Homepage-Banner-Makeup-Reveal--2_30a28cb4-977f-4525-a2c6-e585fc2d3f99_2048x.png?v=1662355920"  />
@@ -28,5 +30,6 @@ export default function Topbanner(){
             {/* <p className="legend">Legend 3</p> */}
         </div>
     </Carousel>
+    </Stack>
     )
 }
